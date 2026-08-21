e13219731cca28901ad54e69b3b6f1641a129089
Prerna Dangara <prernadangara@gmail.com>
2026-08-21 12:20:11 +0530

    Add files via upload
    Add Firebase configuration template

---
+++ config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "YOUR FIREBASE API KEY",
+    authDomain: "YOUR FIREBASE AUTH DOMAIN",
+    projectId: "YOUR FIREBASE PROJECT ID",
+    storageBucket: "YOUR FIREBASE STORAGE BUCKET",
+    messagingSenderId: "YOUR FIREBASE MESSAGING SENDER ID",
+    appId: "YOUR FIREBASE APP ID"
+};
